6587d2b03fc1ab62a16a60fedace08fcae0e83ee
Danshendaisy <37489386+Danshendaisy@users.noreply.github.com>
2019-04-26 17:14:08 -0400

    update

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -1,11 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
-using Fisher.Bookstore.Api.Data.BookstoreContext;
+using Fisher.Bookstore.Api.Data;
 using Fisher.Bookstore.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -18,64 +17,59 @@ namespace Fisher.Bookstore.Api.Controllers
     [Produces("application/json")]
     [Route("api/account")]
     [ApiController]
-    public class AccountController : ControllerBase
-    {
-        private UserManager<ApplicationUser> userManager;
-        private SignInManager<ApplicationUser> signInManager;
-        private IConfiguration configuration;
 
+    public class AccountController : ControllerBase {
 
-        public AccountController(UserManager<ApplicationUser> userManager,
-        SignInManager<ApplicationUser> signInManager,
-        IConfiguration configuration)
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly IConfiguration configuration;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IConfiguration configuration) 
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
             this.configuration = configuration;
         }
-    [HttpPost("register")]
-    public async Task<IActionResult> Register([FromBody] ApplicationUser registration)
-    {
-        if(!ModelState.IsValid)
-        {
-            return BadRequest();
-        }
 
-        ApplicationUser user = new ApplicationUser
-        {
-            Email = registration.Email,
-            UserName = registration.UserName,
-            Id = registration.Id
-        };
-
-        IdentityResult result = await userManager.CreateAsync(user, registration.Password);
-
-        if(!result.Succeeded)
-        {
-            foreach(var err in result.Errors)
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] ApplicationUser registration) {
+            if(!ModelState.IsValid)
             {
-                ModelState.AddModelError(err.Code, err.Description);
+                return BadRequest();
             }
-            return BadRequest(ModelState);
+
+            ApplicationUser user = new ApplicationUser {
+                Email = registration.Email,
+                UserName = registration.Email,
+                Id = registration.Email,
+            };
+
+            IdentityResult result = await userManager.CreateAsync(user, registration.Password);
+
+            if (!result.Succeeded) {
+                foreach (var err in result.Errors) {
+                    ModelState.AddModelError(err.Code, err.Description);
+                }
+                return BadRequest(ModelState);
+            }
+            return Ok();
+            
         }
-        return Ok();
-    }
-    [AllowAnonymous]
-    [HttpPost("login")]
-    public async Task<IActionResult>Login([FromBody] ApplicationUser login)
-    {
-        var result = await signInManager.PasswordSignInAsync(login.Email,
-        login.Password, isPersistent: false, lockoutOnFailure: false);
-        if(!result.Succeeded)
-        {
-            return Unauthorized();
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] ApplicationUser login) {
+            var result = await signInManager.PasswordSignInAsync(login.Email, login.Password, isPersistent: false, lockoutOnFailure: false);
+                if (!result.Succeeded) {
+                    return Unauthorized();
+                }
+
+                ApplicationUser user = await userManager.FindByEmailAsync(login.Email);
+                JwtSecurityToken token = await GenerateTokenAsync(user);
+                string serializedToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+                return Ok();
         }
-        ApplicationUser user = await userManager.FindByEmailAsync(login.Email);
-        JwtSecurityToken token = await GenerateTokenAsync(user);
-        string serializedToken =new JwtSecurityTokenHandler().WriteToken(token);
-        var response = new {Token = serializedToken};
-        return Ok(response);
-    }
 
         private async Task<JwtSecurityToken> GenerateTokenAsync(ApplicationUser user)
         {
@@ -83,33 +77,49 @@ namespace Fisher.Bookstore.Api.Controllers
             {
                 new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim(ClaimTypes.NameIdentifier,user.Id),
                 new Claim(ClaimTypes.Name, user.UserName),
             };
-            
-            var expirationDays = configuration.GetValue<int>
-            ("JWTConfiguration:TokenExpirationDays");
 
-            var signinKey = Encoding.UTF8.GetBytes(configuration.GetValue<string>
-            ("JWTConfiguration:Key"));
+            var expriationDays = configuration.GetValue<int>
+        ("JWTConfiguration:TokenExpirationDays");
+
+            var signinKey = Encoding.UTF8.GetBytes(configuration.GetValue<string>("JWTConfiguration:Key"));
 
             var token = new JwtSecurityToken(
-                issuer:configuration.GetValue<string>("JWTConfiguration:Issuer"),
-                audience:configuration.GetValue<string>("JWTConfiguration:Audience"),
+                issuer: configuration.GetValue<string>("JWTConnfiguration:Issuer"),
+                audience: configuration.GetValue<string>("JWTConfiguration:Audience"),
                 claims: claims,
-                expires: DateTime.UtcNow.Add(TimeSpan.FromDays(expirationDays)),
+                expires: DateTime.UtcNow.Add(TimeSpan.FromDays(expriationDays)),
                 notBefore: DateTime.UtcNow,
-                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(signingKey), SecurityAlgorithms.HmacSha256)
-            );
-            return token;
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(signinKey), SecurityAlgorithms.HmacSha256));
+
+                return token;
         }
 
-    [Authorize]
-    [HttpGet("profile")]
-    public IActionResult Profile()
-    {
-        return Ok(User.Identity.Name);
-    }
-    }   
+        [Authorize]
+        [HttpGet("profile")]
+        public IActionResult Profile() {
+            return Ok(User.Identity.Name);
+        }
 
+        [Authorize]
+        [HttpPut("put")]
+        public IActionResult Put() {
+            return Ok(User.Identity.Name);
+        }
+
+        [Authorize]
+        [HttpPost("post")]
+        public IActionResult Post() {
+            return Ok(User.Identity.Name);
+        }
+
+        [Authorize]
+        [HttpGet("delete")]
+        public IActionResult Delete() {
+            return Ok(User.Identity.Name);
+        }
+    }
+
+ 
 }
